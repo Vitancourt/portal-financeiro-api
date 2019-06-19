bacf0dc6f6cedfe8d0c04e9fcb24cd7f959dc006
webjump-nb083 <pedro.esteves@webjump.com.br>
2019-06-19 17:01:22 -0300

    Post to realizacao-de-negociacao is working with the user cdUnb and cdCliente bound to the document.

--- handlers/realizacaodenegociacao/index.js
+++ handlers/realizacaodenegociacao/index.js
@@ -1,6 +1,8 @@
 const repository = require('../../repository/titulosnegociados')
 const RealizacaoDeNegociacao = require('../../models/realizacaodanegociacao')
+const User = require('../../models/user')
 const router = require('express').Router()
+const mongoose = require('mongoose')
 // Handlers are responsible for managing the request and response objects, and link them to a service module that will do the hard work.
 // Each of the following handlers has the req and res parameters, which stands for request and response. 
 // Each handler of this module represents an HTTP verb (GET, POST, PUT and DELETE) that will be linked to them in the future through a router.
@@ -20,32 +22,47 @@ router.post('/', (req, res, next) => {
         cdFilialLst,
         nrParcelaLst,
         dtEmissaoLst,
-        nrDiasEmAbertoLst
+        nrDiasEmAbertoLst,
+        cdUnb,
+        cdCliente
     } = req.body
 
-    const negociacaoData = new RealizacaoDeNegociacao({
-        qtDiasInadimplencia,
-        dsEmail,
-        tpNegociacao,
-        nrParcelas,
-        idPeriodicidade,
-        idEntrada,
-        nrTituloLst,
-        dtVencimentoLst,
-        vlTituloLst, 
-        cdEmpresaLst,
-        cdFilialLst,
-        nrParcelaLst,
-        dtEmissaoLst,
-        nrDiasEmAbertoLst
+    const user = User.find({ cdUnb: cdUnb, cdCliente: cdCliente }, function (err, users) {
+        if (err) console.error(err)
+        if (users[0] === undefined) {
+            return res.status(500).json({ msg: "User not found!" })
+        }
+        if (cdUnb === users[0].cdUnb && cdCliente === users[0].cdCliente) {
+            const negociacaoData = new RealizacaoDeNegociacao({
+                qtDiasInadimplencia,
+                dsEmail,
+                tpNegociacao,
+                nrParcelas,
+                idPeriodicidade,
+                idEntrada,
+                nrTituloLst,
+                dtVencimentoLst,
+                vlTituloLst, 
+                cdEmpresaLst,
+                cdFilialLst,
+                nrParcelaLst,
+                dtEmissaoLst,
+                nrDiasEmAbertoLst,
+                cdUnb: users[0].cdUnb,
+                cdCliente: users[0].cdCliente
+            })
+            negociacaoData.save()
+                .then(negociacaoDataSaved => {
+                    return res.status(201).json(negociacaoDataSaved)
+                })
+                .catch(err => {
+                    return res.status(500).json({msg: "Cannot save negotiation"})
+                })
+        } else {
+            return res.status(500).json({ msg: "User not found!" })
+        }
+
     })
-    negociacaoData.save()
-        .then(negociacaoDataSaved => {
-            return res.status(201).json(negociacaoDataSaved)
-        })
-        .catch(err => {
-            return res.status(500).json({msg: "Cannot save negotiation"})
-        })
 })
 
 module.exports = router
