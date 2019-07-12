2dac4dd15841a1ad30d380d360da71b63d933e92
mahsum <mahsum@webjump.com.br>
2019-07-12 16:38:23 -0300

    check if cdunb and cdclient is integer

--- handlers/formasdepagamento/index.js
+++ handlers/formasdepagamento/index.js
@@ -54,7 +54,7 @@ router.post('/', (req, res, next) => {
     const user = User.find({cdUnb: cdUnb, cdCliente: cdCliente}, function(err, users) {
         if(err) return console.log(err)
         if(users[0] === undefined) return res.status(500).json({msg: "User not found"})
-        if(cdUnb === users[0].cdUnb && cdCliente === users[0].cdCliente) {
+        if(parseInt(cdUnb) === users[0].cdUnb && parseInt(cdCliente) === users[0].cdCliente) {
             const formadepagamentos = FormaDePagamento({
                 Parcelamento: [
                     {
